266b767e350de5c403a0d160812b38e101672870
IvyJuly7 <88373209+IvyJuly7@users.noreply.github.com>
2021-08-26 17:23:45 +1000

    Delete Proof of concept directory

--- Proof of concept/README.markdown
+++
@@ -1,5 +0,0 @@
-# 
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/ivyjuly7/pen/XWRPyLo](https://codepen.io/ivyjuly7/pen/XWRPyLo).
-
-
--- Proof of concept/src/script.js
+++
@@ -1,5 +0,0 @@
-function to() {
-  var search = document.getElementById("searchInput").value;
-  // alert(searchValue);
-  window.location.href = "" + search;
-}
